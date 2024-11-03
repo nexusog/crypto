3df839184b2772705db90fdc9752c9053426bab0
Henil Malaviya <henilm.dev@gmail.com>
2024-11-03 17:33:47 +0530

    fix: add equal conditions

--- test/utils/getRandomString.test.ts
+++ test/utils/getRandomString.test.ts
@@ -1,13 +1,13 @@
-import { describe, expect, test } from "vitest";
-import NexusCrypto from "@/index";
+import { describe, expect, test } from 'vitest'
+import NexusCrypto from '@/index'
 
-describe("getRandomString", () => {
-  test("light", () => {
-    const simpleTest = NexusCrypto.utils.getRandomString(5);
+describe('getRandomString', () => {
+	test('light', () => {
+		const simpleTest = NexusCrypto.utils.getRandomString(5)
 
-    expect(simpleTest).toBeDefined();
-    expect(simpleTest.length).toBe(5);
-    expect(simpleTest.charCodeAt(0)).toBeGreaterThan(0);
-    expect(simpleTest.charCodeAt(0)).toBeLessThan(256);
-  });
-});
+		expect(simpleTest).toBeDefined()
+		expect(simpleTest.length).toBe(5)
+		expect(simpleTest.charCodeAt(0)).toBeGreaterThanOrEqual(0)
+		expect(simpleTest.charCodeAt(0)).toBeLessThanOrEqual(256)
+	})
+})
--- test/utils/getRandomUint8Array.test.ts
+++ test/utils/getRandomUint8Array.test.ts
@@ -1,13 +1,13 @@
-import { describe, expect, test } from "vitest";
-import NexusCrypto from "@/index";
+import { describe, expect, test } from 'vitest'
+import NexusCrypto from '@/index'
 
-describe("getRandomUint8Array", () => {
-  test("light", () => {
-    const simpleTest = NexusCrypto.utils.getRandomUint8Array(5);
+describe('getRandomUint8Array', () => {
+	test('light', () => {
+		const simpleTest = NexusCrypto.utils.getRandomUint8Array(5)
 
-    expect(simpleTest).toBeDefined();
-    expect(simpleTest.length).toBe(5);
-    expect(simpleTest[0]).toBeGreaterThan(0);
-    expect(simpleTest[0]).toBeLessThan(256);
-  });
-});
+		expect(simpleTest).toBeDefined()
+		expect(simpleTest.length).toBe(5)
+		expect(simpleTest[0]).toBeGreaterThanOrEqual(0)
+		expect(simpleTest[0]).toBeLessThanOrEqual(256)
+	})
+})
